86be3fba3520aad7037e60fdf842a4b3e9a7a539
Muro472 <73928271+Muro472@users.noreply.github.com>
2020-11-30 21:38:40 +0200

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,19 @@
-n = input('Введіть число n:')
-b = list(n)
-len = len(n)
-i = 0
-list1 = ''
-while len > i:
-    list1 = str(b[i]) + list1
-    i += 1
-print(list1)
+n1 = int(input('Введіть число n:'))
+i = 1
+k = 1
+n2 = 0
+length = -2
+while i != 0:
+    i = n1 // k
+    k = k * 10#знаходження довжини числа
+    length += 1
+a = 10 ** length
+i = 1
+p = 1
+while i != 0:
+    i = n1 // a
+    n1 = n1 - (a * i)
+    a = a / 10
+    n2 = (i * p) + n2
+    p = p * 10
+print(n2)
